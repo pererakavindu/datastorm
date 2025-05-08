92cddc493b33fdc30ff1cc4f8c1e767754b3ad54
mani-hash <manimehalan400@gmail.com>
2025-05-08 17:27:23 +0530

    added classification table

--- dashboard/classication.py
+++ dashboard/classication.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown("# Classification page")
 st.sidebar.markdown("# Classification page")
+
+classificationDataFrame = pd.read_csv('selected_output.csv')
+
+# Optional: Rename columns for nicer display
+classificationDataFrame = classificationDataFrame.rename(columns={
+    'agent_code': 'Agent Code',
+    'performance': 'Performance'
+})
+
+# Display with Streamlit
+st.dataframe(classificationDataFrame, use_container_width=True, height=600)
